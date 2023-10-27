188e3a70785446af9af767e7e39677c492cf2cc1
mMm1m <104731296+mMm1m@users.noreply.github.com>
2023-10-28 02:30:44 +0300

    Revert "Parser"

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
-import controller.RequestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import service.LabyrinthITParsingImpl;
 
 import java.io.IOException;
 
@@ -12,5 +11,20 @@ import java.io.IOException;
 public class Demo1Application {
     public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(Demo1Application.class, args);
+        RequestController controller = new RequestController();
+        String curr = controller.getHTMLFromHTTPClient();
+        String it_book = "https://www.labirint.ru/books/512969/";
+        String eng_book = "https://www.labirint.ru/books/263504/";
+        var server = new LibraryServer();
+        var b = server.parseHTML(it_book);
+        for(var a : b.getFirst())
+        {
+            System.out.println(a);
+        }
+        System.out.println();
+        for(var a : b.getSecond())
+        {
+            System.out.println(a);
+        }
     }
 }
--- src/main/java/com/example/demo/RequestController.java
+++ src/main/java/com/example/demo/RequestController.java
@@ -1,11 +1,7 @@
-package controller;
+package com.example.demo;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.Getter;
-import lombok.Setter;
-import org.json.simple.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.HttpConnection;
@@ -17,11 +13,6 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UrlPathHelper;
-import service.LabyrinthITParsingImpl;
-import service.LabyrinthParsingHtml;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
@@ -37,16 +28,26 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 @RestController
-@Getter
-@Setter
+@RequestMapping("/something/path")
 public class RequestController {
-    private Document document = null;
-    private String url = null;
-    public RequestController(){}
-    @RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
-    public void getURL(HttpServletRequest request) throws IOException {
-        this.url = request.getRequestURL().toString();
-        this.document = Jsoup.connect(url).get();
-        System.out.println();
+
+    // get html from url with 2 methods
+    @GetMapping("/next")
+    public String getHtml(HttpServletRequest request) throws IOException {
+        String urlInTextFormat = request.getRequestURL().toString();
+        return Jsoup.connect(urlInTextFormat).get().html();
     }
+
+    @GetMapping
+    public String getHTMLFromHTTPClient() throws IOException, InterruptedException {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://www.labirint.ru/books/512969/"))
+                    .GET() // GET is default
+                    .build();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            return response.body();
+    }
+
 }
